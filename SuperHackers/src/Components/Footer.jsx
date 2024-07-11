c1f2188739cef845bf1fac8b7b457f995bb2a759
Amanda Perry <challenge641@gmail.com>
2024-07-11 16:48:20 -0500

    Added Home, Events, About Pages. Moved Nav component and added footer component

---
+++ SuperHackers/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+
+  return (
+    <>
+     <h6>FB</h6>
+     <h6>IG</h6>
+     <h6>X</h6>
+     <h6>Snap</h6>
+    </>
+  )
+}
+
+export default Footer;
---
+++ SuperHackers/src/Pages/Events.jsx
@@ -0,0 +1,13 @@
+
+function Events(){
+
+  return (
+    <>
+    <div>
+      <h1>Event Calendar</h1>
+    </div>
+    </>
+  )
+}
+
+export default Events;
